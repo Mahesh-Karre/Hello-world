a25a460c55ddae0769c97e497a7a5ac8e2c83d7f
Mahesh-Karre <39051105+Mahesh-Karre@users.noreply.github.com>
2018-05-08 15:10:52 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Vowel
+{
+	static public void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the char");
+		char ch=sc.next().charAt(0);
+
+		if(ch=='A'||ch=='E'||ch=='I'||ch=='o'||ch=='U'||ch=='a'||ch=='i'||ch=='o'||ch=='e'||ch=='u')
+		{
+			System.out.println("vowel");	
+		}
+
+	
+		else if
+		{		
+			System.out.println("consonent");
+		}
+	}
+		
+}	
